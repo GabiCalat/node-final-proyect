9b9285c8e0c2edb282aebac1cc4e797c1f6cfc8f
dosorio55 <dosorio55@gmail.com>
2022-07-13 09:17:56 +0200

    quitado del required del loco de las compañías

--- server/api/models/companies.models.js
+++ server/api/models/companies.models.js
@@ -9,7 +9,7 @@ const CompaniesSchema = new Schema(
         name_job: { type: String, required: true },
         companie: { type: String, required: true },
         cif: { type: String, required: true },
-        logo: { type: String, required: true },
+        logo: { type: String },
 
     });
 

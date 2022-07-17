2d169535f9997d3b42de1d88a6ac29bf5fcd3f88
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-07-17 11:00:20 +0200

    cloudinary users modelo ok

--- server/api/models/User.Model.js
+++ server/api/models/User.Model.js
@@ -5,8 +5,8 @@ const userSchema = new Schema(
   {
     name: { type: String, required: false },
     surname: { type: String, required: false },
-    email: { type: String, required: false },
-    password:{type:String,required:false},
+    email: { type: String, required: true },
+    password:{type:String,required:true},
     education: { type: String},
     age: { type: Number, required: false },
     description:{type:String,required:false},

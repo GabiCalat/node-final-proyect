619d25713a0da630c7213421db8a00ebf49a250c
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-07-19 18:19:13 +0200

    subscribe user to offer job and repair form jobs

--- server/api/routes/jobs.routes.js
+++ server/api/routes/jobs.routes.js
@@ -1,11 +1,12 @@
 import expres from "express";
-import { getAllJobs,getJobById,createJob} from "../controllers/job.controllers.js";
+import { getAllJobs,getJobById,createJob, addUserToJob} from "../controllers/job.controllers.js";
 
 const jobRoutes = expres.Router();
 
 jobRoutes.get("/", getAllJobs);
 jobRoutes.get("/:id", getJobById);
 jobRoutes.post("/", createJob);
+jobRoutes.put("/add-user", addUserToJob);
 // jobRoutes.get("/name/:name", findjobByName);
 // jobRoutes.put("/modify/:id", editNamejob);
 

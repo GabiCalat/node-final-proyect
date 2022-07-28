39c37fc26980621a43bd714b3673a54016d2b09a
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-07-28 17:34:36 +0200

    username required false

--- server/api/models/user.model.js
+++ server/api/models/user.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    surname: { type: String, required: true },
+    surname: { type: String, required: false },
     email: { type: String, required: true },
     password: { type: String, required: true },
     account_type: { type: String, required: true },
